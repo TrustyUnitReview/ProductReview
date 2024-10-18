a661f24fc9a9d164318c20302a28022b08e7ae66
Juanita Rodelo <juanitarodeloo@gmail.com>
2024-10-18 09:19:19 -0400

    Initial commit

---
+++ src/main/java/org/products/productreviews/ProductReviewsApplication.java
@@ -0,0 +1,13 @@
+package org.products.productreviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductReviewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductReviewsApplication.class, args);
+    }
+
+}
