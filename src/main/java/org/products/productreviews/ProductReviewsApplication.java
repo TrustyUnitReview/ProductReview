3f547550b35c310c5635e833c9ed377c599d027c
Juanita Rodelo <juanitarodeloo@gmail.com>
2024-10-21 15:04:26 -0400

    Project init

--- src/main/java/org/products/productreviews/ProductReviewsApplication.java
+++ src/main/java/org/products/productreviews/ProductReviewsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProductReviewsApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(ProductReviewsApplication.class, args);
     }
