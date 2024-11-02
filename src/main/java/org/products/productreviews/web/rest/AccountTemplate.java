82a07846818f38fc724e7bb054b8a5c9ed0b65c0
Alexandre Marques <alex.marques@live.ca>
2024-11-02 17:00:48 -0400

    Fix old no param constructor calls.
    - Replaced with factory call
    - Factory requires passing the repo, which might be an insidious issue. Marked as a TODO to look at.
    - Factory also requires a try-catch, leaving error handling implementation for later.

--- src/main/java/org/products/productreviews/web/rest/AccountTemplate.java
+++ src/main/java/org/products/productreviews/web/rest/AccountTemplate.java
@@ -1,6 +1,8 @@
 package org.products.productreviews.web.rest;
 
 import org.products.productreviews.app.Entities.User;
+import org.products.productreviews.app.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,9 +10,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 
+import java.util.Arrays;
+
 @Controller
 @RequestMapping("/user")
 public class AccountTemplate {
+    // TODO: Inspect this, is there a better way to do this?
+    //  Removing repo from createUser would fix the issue at the root.
+    @Autowired
+    private UserRepository repo;
+
 
     @GetMapping("/{id}")
     public String viewAccount(@PathVariable long id, Model model){
@@ -23,7 +32,14 @@ public class AccountTemplate {
 
     @GetMapping("/reg")
     public String userRegistration(Model model) {
-        model.addAttribute("newUser", new User());
+        // TODO: Add input for username, password
+        try{
+            model.addAttribute("newUser", User.createUser(repo, "username", "password"));
+        }
+        catch (Exception e){
+            // TODO: Better error handler, have a way to retry registration? -> Different behavior based on type?
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        }
         return "userRegistration";
     }
 
