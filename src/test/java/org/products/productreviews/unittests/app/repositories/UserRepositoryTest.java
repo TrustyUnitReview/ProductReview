aa4196125a1988aa38dd4b642a03f32859ca9b86
Alexandre Marques <alex.marques@live.ca>
2024-11-04 20:25:31 -0500

    Add a (WIP) test setup to UserRepositoryTest for the DB
    - Doesn't actually work yet, required to get the entity tests to work.
    - Related to issue #10, might take a break for now until there's progress on that.

--- src/test/java/org/products/productreviews/unittests/app/repositories/UserRepositoryTest.java
+++ src/test/java/org/products/productreviews/unittests/app/repositories/UserRepositoryTest.java
@@ -1,6 +1,8 @@
 package org.products.productreviews.unittests.app.repositories;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.products.productreviews.app.entities.User;
 import org.products.productreviews.app.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,13 +14,18 @@ class UserRepositoryTest {
     @Autowired
     private UserRepository repo;
 
+    @BeforeEach
+    void setUp() throws Exception {
+        // TODO: Figure out how to select for this test a DB which contains testUser1 (and not userNotInDB).
+        User validUser = User.createUser(repo,"testUser1", "p1");
+    }
+
     /**
      * This test checks if the existsByUsername works as intended.
      * That is, returns true if the username exists in the DB, and false otherwise.
      */
     @Test
     void existsByUsername() {
-        // TODO: Figure out how to select for this test a DB which contains testUser1 (and not userNotInDB).
         assertTrue(repo.existsByUsername("testUser1"));
         assertFalse(repo.existsByUsername("userNotInDB"));
     }
