dfc67bdf2b266ad1f5ca01003c779e6a0f962bbb
orselane <77144245+orselane@users.noreply.github.com>
2024-11-05 13:45:33 -0500

    Add Product Entity.
    - Added required fields (Review relation broken for now until it becomes an entity)
    - Created factory method
    - Left some business logic methods unimplemented for now.

---
+++ src/main/java/org/products/productreviews/app/entities/Product.java
@@ -0,0 +1,100 @@
+package org.products.productreviews.app.entities;
+
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import jakarta.persistence.*;
+import org.products.productreviews.app.Review;
+import org.products.productreviews.app.repositories.ProductRepository;
+
+import java.awt.*;
+import java.security.InvalidKeyException;
+import java.util.Set;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue
+    private Long id;
+    // Should this be the PK?
+    @Column(unique=true)
+    private String name;
+    private String description;
+    // Cannot store Image or Path objects directly, required to store string to load later
+    private String imagePath;
+    @Transient
+    private Image loadedImage;
+    @ElementCollection
+    private Set<String> sellerLinks;
+    // FIXME: Requires review to be an entity.
+    @OneToMany
+    private Set<Review> reviews;
+
+    /**
+     * Protected constructor used by JPA for reflexive construction.
+     */
+    protected Product(){
+        loadImage();
+    }
+
+    /**
+     * Private constructor used by factory. See factory method for more details.
+     *
+     * @param name The name of the product.
+     * @param description The description of the product.
+     * @param imagePath The filepath given as a String.
+     */
+    private Product(String name, String description, String imagePath){
+        this.name = name;
+        this.description = description;
+        this.imagePath = imagePath;
+        loadImage(); // Cannot re-use protected constructor, has to be called after imagePath is set.
+    }
+
+    /**
+     * Input sanitation for product creation, allows for creation failure with exception throws.
+     * This factory is currently the only intended way to create Products.
+     *
+     * @param repo The ProductRepository reference.
+     * @param name The name of the product.
+     * @param description The description of the product.
+     * @param imagePath The filepath given as a String.
+     *
+     * @return A new product object.
+     *
+     * @throws InvalidKeyException If the product name already exists (must be unique).
+     * @throws InvalidFormatException If a description, or imagePath does not follow a proper format
+     *  (see "check..." helper methods for format specifications).
+     */
+    public static Product createProduct(ProductRepository repo, String name, String description, String imagePath)
+            throws InvalidKeyException, InvalidFormatException {
+        if (repo.existsByName(name)){
+            throw new InvalidKeyException("Name already exists");
+        }
+        checkDescriptionFormat();
+        checkImagePath();
+
+        return new Product(name, description, imagePath);
+    }
+
+    private static void checkDescriptionFormat() throws InvalidFormatException {
+    }
+
+    private static void checkImagePath() throws InvalidFormatException {
+        // TODO: Implement this, throw error on fail
+    }
+
+    private void loadImage() {
+        // TODO: Implement this, set loadedImage using imagePath
+    }
+
+    public float getReviewScore(){
+        // TODO: Implement scoring algorithm... May take in a strategy later
+        return 0.0f; // Between 1-5
+    }
+
+    /**
+     * @return The image representing the product.
+     */
+    public Image getImage(){
+        return loadedImage;
+    }
+}
