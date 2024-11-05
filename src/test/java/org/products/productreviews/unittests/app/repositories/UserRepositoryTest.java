f95f8badd979327b3b5472012a2698ed52ea91cd
Alexandre Marques <alex.marques@live.ca>
2024-11-04 19:16:56 -0500

    Add UserRepositoryTest.
    - It's missing the selection of the database, and a sample database. Related to issue #10.

---
+++ src/test/java/org/products/productreviews/unittests/app/repositories/UserRepositoryTest.java
@@ -0,0 +1,25 @@
+package org.products.productreviews.unittests.app.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.products.productreviews.app.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserRepositoryTest {
+    @Autowired
+    private UserRepository repo;
+
+    /**
+     * This test checks if the existsByUsername works as intended.
+     * That is, returns true if the username exists in the DB, and false otherwise.
+     */
+    @Test
+    void existsByUsername() {
+        // Now, just figure out how to select for this test a DB which contains testUser1 (and not userNotInDB).
+        assertTrue(repo.existsByUsername("testUser1"));
+        assertFalse(repo.existsByUsername("userNotInDB"));
+    }
+}
