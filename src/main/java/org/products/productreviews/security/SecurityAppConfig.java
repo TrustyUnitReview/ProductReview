aae5970bcd7432e4d2b3aad44970c5b875ed0883
Joshua Braddon <jabraddon@gmail.com>
2024-11-13 19:10:04 -0500

    Added default user

--- src/main/java/org/products/productreviews/security/SecurityAppConfig.java
+++ src/main/java/org/products/productreviews/security/SecurityAppConfig.java
@@ -2,8 +2,12 @@ package org.products.productreviews.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,15 +23,8 @@ public class SecurityAppConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/registration").permitAll()
-                        .anyRequest().authenticated()
-                );
-//                .formLogin((form) -> form
-//                        .loginPage("/login")
-//                        .permitAll()
-//                )
-//                .logout((logout) -> logout.permitAll());
+                .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated()
+                ).httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
