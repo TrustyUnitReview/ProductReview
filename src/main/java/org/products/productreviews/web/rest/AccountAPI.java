08caf447e79c4bb8d74478815d8a9d03252e08c3
Yasir Sheikh <ysheikh1234@gmail.com>
2024-11-21 16:03:17 -0500

    Post methods for following and unfollowing an account

--- src/main/java/org/products/productreviews/web/rest/AccountAPI.java
+++ src/main/java/org/products/productreviews/web/rest/AccountAPI.java
@@ -1,49 +1,71 @@
 package org.products.productreviews.web.rest;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.products.productreviews.ProductReviewsApplication;
 import org.products.productreviews.app.entities.Account;
+import org.products.productreviews.app.repositories.AccountRepository;
+import org.products.productreviews.web.util.WebUtil;
 import org.products.productreviews.web.util.patcher.Patcher;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.logging.Level;
 
+/**
+ * REST controller for managing accounts
+ */
 @Controller
 @RequestMapping("/account")
 public class AccountAPI {
 
-    @PatchMapping("/{id}")
-    public ResponseEntity<String> editAccount(@PathVariable long id, @RequestBody Account partialAccount) {
-        Account currentAccount = null;
-        // find user from userRepo by id and ensure user matches with authenticated user
-        // if user does not match, don't patch
-        if (currentAccount != null) {
-            try {
-                Patcher.patch(currentAccount, partialAccount); //this approach is simple, but maybe DTO pattern
-                //save to repo
-            } catch (IllegalAccessException e) {
-                ProductReviewsApplication.LOGGER.log(Level.SEVERE, Arrays.toString(e.getStackTrace()));
-                return ResponseEntity.badRequest().build();
-            }
-            return ResponseEntity.ok().build();
-        }
-        return ResponseEntity.badRequest().build();
+    private final AccountRepository accountRepo;
+
+    /**
+     * Constructor for initializing account repository
+     *
+     * @param accountRepo The repository for accounts
+     */
+    public AccountAPI(AccountRepository accountRepo) {
+        this.accountRepo = accountRepo;
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteAccount(@PathVariable long id) {
+    /**
+     * Updates an account to add a following to another user
+     *
+     * @param username of the account to be followed
+     * @param request HttpServletRequest reference
+     * @return The page to be served upon following an account
+     */
+    @PostMapping("/{username}/follow")
+    public String followAccount(@PathVariable("username") String username, HttpServletRequest request) {
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        Account account = accountRepo.findByUsername(authentication.getName());
+        Account otherAccount = accountRepo.findByUsername(username);
 
-        // find account by id
-        Account account = null;
-        //authenticate  current account with found account
-        // if account does not match, don't delete
-        if (account != null) {
-            //userRepo.deleteById(id)
-            return ResponseEntity.ok().build();
-        }
-        return ResponseEntity.badRequest().build();
+//        account.addFollows(otherAccount);
+//        accountRepo.save(account);
+        return WebUtil.getPreviousPageByRequest(request).orElse("/dashboard");
     }
 
+    /**
+     * Updates an account to remove a user from following
+     *
+     * @param username of the account to be unfollowed
+     * @param request HttpServletRequest reference
+     * @return The page to be served upon unfollowing an account
+     */
+    @PostMapping("/{username}/unfollow")
+    public String unfollowAccount(@PathVariable("username") String username, HttpServletRequest request) {
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        Account account = accountRepo.findByUsername(authentication.getName());
+        Account otherAccount = accountRepo.findByUsername(username);
+
+//        account.removeFollows(otherAccount);
+//        accountRepo.save(account);
+        return WebUtil.getPreviousPageByRequest(request).orElse("/dashboard");
+    }
 }
