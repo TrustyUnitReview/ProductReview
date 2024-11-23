38167533c4666a790014d1cb5fcc10cf4b5d9916
Yasir Sheikh <ysheikh1234@gmail.com>
2024-11-23 17:04:43 -0500

    Updating API and template class work with authenticated user

--- src/main/java/org/products/productreviews/web/rest/AccountAPI.java
+++ src/main/java/org/products/productreviews/web/rest/AccountAPI.java
@@ -1,19 +1,19 @@
 package org.products.productreviews.web.rest;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.products.productreviews.ProductReviewsApplication;
 import org.products.productreviews.app.entities.Account;
 import org.products.productreviews.app.repositories.AccountRepository;
 import org.products.productreviews.web.util.WebUtil;
-import org.products.productreviews.web.util.patcher.Patcher;
-import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.logging.Level;
+import java.util.Optional;
 
 /**
  * REST controller for managing accounts
@@ -42,13 +42,17 @@ public class AccountAPI {
      */
     @PostMapping("/{username}/follow")
     public String followAccount(@PathVariable("username") String username, HttpServletRequest request) {
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        Account account = accountRepo.findByUsername(authentication.getName());
-        Account otherAccount = accountRepo.findByUsername(username);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Optional<Account> account = accountRepo.findByUsername(authentication.getName());
+        Optional<Account> otherAccount = accountRepo.findByUsername(username);
 
-//        account.addFollows(otherAccount);
-//        accountRepo.save(account);
-        return WebUtil.getPreviousPageByRequest(request).orElse("/dashboard");
+        if (account.isPresent() && otherAccount.isPresent()) {
+            account.get().addFollows(otherAccount.get());
+            accountRepo.save(account.get());
+            return WebUtil.getPreviousPageByRequest(request).orElse("/dashboard");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Account not found").toString();
+        }
     }
 
     /**
@@ -60,12 +64,16 @@ public class AccountAPI {
      */
     @PostMapping("/{username}/unfollow")
     public String unfollowAccount(@PathVariable("username") String username, HttpServletRequest request) {
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        Account account = accountRepo.findByUsername(authentication.getName());
-        Account otherAccount = accountRepo.findByUsername(username);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Optional<Account> account = accountRepo.findByUsername(authentication.getName());
+        Optional<Account> otherAccount = accountRepo.findByUsername(username);
 
-//        account.removeFollows(otherAccount);
-//        accountRepo.save(account);
-        return WebUtil.getPreviousPageByRequest(request).orElse("/dashboard");
+        if (account.isPresent() && otherAccount.isPresent()) {
+            account.get().removeFollows(otherAccount.get());
+            accountRepo.save(account.get());
+            return WebUtil.getPreviousPageByRequest(request).orElse("/dashboard");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Account not found").toString();
+        }
     }
 }
