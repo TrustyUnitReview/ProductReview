23e388027f9c9b06f96a6d266786ed57488bf156
Alexandre Marques <alex.marques@live.ca>
2024-11-24 12:32:45 -0500

    Add sample users for testing

--- src/main/java/org/products/productreviews/ProductReviewsApplication.java
+++ src/main/java/org/products/productreviews/ProductReviewsApplication.java
@@ -2,6 +2,7 @@ package org.products.productreviews;
 
 import org.products.productreviews.app.entities.Account;
 import org.products.productreviews.app.entities.Product;
+import org.products.productreviews.app.entities.Review;
 import org.products.productreviews.app.repositories.AccountRepository;
 import org.products.productreviews.app.repositories.ProductRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -36,45 +37,45 @@ public class ProductReviewsApplication {
             Account admin = Account.createAccount(accountRepository, "username", "MyPassword1!");
             accountRepository.save(admin);
             Product example = Product.createProduct(productRepository,
-                "FountainPen",
-                399.99f,
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam efficitur placerat " +
-                                "magna quis porta. Praesent eget urna ac felis egestas faucibus. Suspendisse dolor enim," +
-                                " fringilla vitae erat in, auctor suscipit nibh. Nunc sed felis finibus, fringilla dui " +
-                                "sagittis, pharetra augue. Fusce vel nulla varius, sagittis nibh ac, porta erat. Integer" +
-                                " arcu leo, aliquet eu turpis eget, semper convallis leo. Praesent iaculis sed diam in " +
-                                "rutrum. Curabitur blandit dui odio, sed eleifend neque laoreet eu. Pellentesque " +
-                                "habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. " +
-                                "Curabitur hendrerit est tortor. Nullam porta lorem felis, et consectetur nisi placerat " +
-                                "sit amet. Proin sapien est, aliquet rutrum neque quis, viverra placerat metus. Proin " +
-                                "lacinia lectus pretium blandit egestas Quisque vel sapien varius, rutrum odio nec, " +
-                                "lobortis tellus. Duis malesuada fringilla nulla eu ultricies. Proin eu malesuada est. " +
-                                "Etiam fermentum, nulla nec ornare iaculis, purus ante egestas nibh, id rhoncus ipsum " +
-                                "augue nec enim. Aliquam purus ligula, faucibus quis orci non, laoreet efficitur quam. " +
-                                "Integer rhoncus purus quam, at laoreet magna interdum consectetur. Nullam in metus vel " +
-                                "magna facilisis pulvinar ac eu orci. Donec et pulvinar orci, a ullamcorper lectus. " +
-                                "Sed a ex mauris. Curabitur ut felis urna. Praesent blandit faucibus quam Donec sit " +
-                                "amet hendrerit mauris. In congue varius nisi, sed posuere lectus iaculis in. Mauris " +
-                                "et purus lacinia, interdum magna eu, volutpat arcu. Praesent malesuada ullamcorper " +
-                                "scelerisque. Curabitur accumsan eros et ex iaculis pellentesque. Nam id nisi nec orci " +
-                                "porta volutpat. In varius, turpis sed ultricies blandit, magna augue pretium metus, " +
-                                "at eleifend sapien massa ac velit. Vestibulum bibendum eros erat, sit amet lobortis " +
-                                "dui venenatis sed Donec tempor aliquam justo, in aliquam urna ultrices nec. Interdum " +
-                                "et malesuada fames ac ante ipsum primis in faucibus. Sed at aliquam ante. Sed nec " +
-                                "odio eget nunc ornare dapibus ornare a magna. Quisque vitae felis vel lacus facilisis " +
-                                "hendrerit. Vestibulum quis augue aliquam, gravida tortor sit amet, fringilla sem. Sed " +
-                                "eget mi neque. Nunc porttitor justo sit amet nulla feugiat, vitae auctor nisi " +
-                                "interdum. Mauris ac tellus ac leo pretium blandit efficitur sed mauris. Nunc " +
-                                "consequat est eu semper dictum. Curabitur consequat, ante at hendrerit pulvinar, " +
-                                "magna dui consequat dolor, vel egestas urna tellus eget nunc. Nullam eget neque quis " +
-                                "ligula dignissim gravida ut at sapien. Orci varius natoque penatibus et magnis dis " +
-                                "parturient montes, nascetur ridiculus mus  In luctus neque ac fringilla eleifend. Sed " +
-                                "ante tortor, mattis vel nulla et, consequat bibendum nisl. Nunc augue neque, placerat " +
-                                "vitae accumsan sed, sollicitudin sed sem. Aliquam sit amet magna enim. In tellus mi, " +
-                                "tincidunt vel consectetur quis, semper vel mauris. Mauris a quam vel lectus tristique " +
-                                "eleifend a nec nisi. Nam non porta sem. Fusce sit amet maximus eros. Sed eget tellus " +
-                                "lectus. Sed luctus sapien ac vestibulum viverra. ",
-                null);
+                    "FountainPen",
+                    399.99f,
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam efficitur placerat " +
+                            "magna quis porta. Praesent eget urna ac felis egestas faucibus. Suspendisse dolor enim," +
+                            " fringilla vitae erat in, auctor suscipit nibh. Nunc sed felis finibus, fringilla dui " +
+                            "sagittis, pharetra augue. Fusce vel nulla varius, sagittis nibh ac, porta erat. Integer" +
+                            " arcu leo, aliquet eu turpis eget, semper convallis leo. Praesent iaculis sed diam in " +
+                            "rutrum. Curabitur blandit dui odio, sed eleifend neque laoreet eu. Pellentesque " +
+                            "habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. " +
+                            "Curabitur hendrerit est tortor. Nullam porta lorem felis, et consectetur nisi placerat " +
+                            "sit amet. Proin sapien est, aliquet rutrum neque quis, viverra placerat metus. Proin " +
+                            "lacinia lectus pretium blandit egestas Quisque vel sapien varius, rutrum odio nec, " +
+                            "lobortis tellus. Duis malesuada fringilla nulla eu ultricies. Proin eu malesuada est. " +
+                            "Etiam fermentum, nulla nec ornare iaculis, purus ante egestas nibh, id rhoncus ipsum " +
+                            "augue nec enim. Aliquam purus ligula, faucibus quis orci non, laoreet efficitur quam. " +
+                            "Integer rhoncus purus quam, at laoreet magna interdum consectetur. Nullam in metus vel " +
+                            "magna facilisis pulvinar ac eu orci. Donec et pulvinar orci, a ullamcorper lectus. " +
+                            "Sed a ex mauris. Curabitur ut felis urna. Praesent blandit faucibus quam Donec sit " +
+                            "amet hendrerit mauris. In congue varius nisi, sed posuere lectus iaculis in. Mauris " +
+                            "et purus lacinia, interdum magna eu, volutpat arcu. Praesent malesuada ullamcorper " +
+                            "scelerisque. Curabitur accumsan eros et ex iaculis pellentesque. Nam id nisi nec orci " +
+                            "porta volutpat. In varius, turpis sed ultricies blandit, magna augue pretium metus, " +
+                            "at eleifend sapien massa ac velit. Vestibulum bibendum eros erat, sit amet lobortis " +
+                            "dui venenatis sed Donec tempor aliquam justo, in aliquam urna ultrices nec. Interdum " +
+                            "et malesuada fames ac ante ipsum primis in faucibus. Sed at aliquam ante. Sed nec " +
+                            "odio eget nunc ornare dapibus ornare a magna. Quisque vitae felis vel lacus facilisis " +
+                            "hendrerit. Vestibulum quis augue aliquam, gravida tortor sit amet, fringilla sem. Sed " +
+                            "eget mi neque. Nunc porttitor justo sit amet nulla feugiat, vitae auctor nisi " +
+                            "interdum. Mauris ac tellus ac leo pretium blandit efficitur sed mauris. Nunc " +
+                            "consequat est eu semper dictum. Curabitur consequat, ante at hendrerit pulvinar, " +
+                            "magna dui consequat dolor, vel egestas urna tellus eget nunc. Nullam eget neque quis " +
+                            "ligula dignissim gravida ut at sapien. Orci varius natoque penatibus et magnis dis " +
+                            "parturient montes, nascetur ridiculus mus  In luctus neque ac fringilla eleifend. Sed " +
+                            "ante tortor, mattis vel nulla et, consequat bibendum nisl. Nunc augue neque, placerat " +
+                            "vitae accumsan sed, sollicitudin sed sem. Aliquam sit amet magna enim. In tellus mi, " +
+                            "tincidunt vel consectetur quis, semper vel mauris. Mauris a quam vel lectus tristique " +
+                            "eleifend a nec nisi. Nam non porta sem. Fusce sit amet maximus eros. Sed eget tellus " +
+                            "lectus. Sed luctus sapien ac vestibulum viverra. ",
+                    null);
             Product example1 = Product.createProduct(productRepository,
                     "FountainPen2",
                     399.99f,
@@ -95,6 +96,31 @@ public class ProductReviewsApplication {
             productRepository.save(example2);
             productRepository.save(example3);
 
+
+            // Create Accounts
+            Account account1 = Account.createAccount(accountRepository,
+                    "user1",
+                    "Pass1234!");
+            accountRepository.save(account1);
+            Account account2 = Account.createAccount(accountRepository,
+                    "user2",
+                    "Pass1234!");
+            account2.addFollows(account1);
+            accountRepository.save(account2);
+
+            // Extra sample users
+            Account account3 = Account.createAccount(accountRepository,
+                    "user3",
+                    "Pass1234!");
+            accountRepository.save(account3);
+            Account account4 = Account.createAccount(accountRepository,
+                    "user4",
+                    "Pass1234!");
+            accountRepository.save(account4);
+            Account account5 = Account.createAccount(accountRepository,
+                    "user5",
+                    "Pass1234!");
+            accountRepository.save(account5);
         };
     }
 
